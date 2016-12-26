9816e7b60e860a9b804e8ca416ca038c0838f542
Frank <wanyefrank@live.com>
2016-12-26 14:45:29 -0500

    Added Procfile, .env and app.json

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node quarto-router.js
--- quarto-router.js
+++ quarto-router.js
@@ -2,12 +2,12 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var hostname = "quarto-arena.herokuapp.com";
-var port = 5000;
 
 var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(port, hostname, function() {
-    console.log("Server running at http://" + hostname + ":" + port);
+var server = app.listen(process.env.PORT || 8080, function() {
+    var port = server.address().port;
+    console.log("Server running on port: " + port);
 });
